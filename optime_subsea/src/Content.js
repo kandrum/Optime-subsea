14e7118d83019941b428ac1486b6c4448d861b79
kandrum <kandrum5418@uhcl.edu>
2024-03-05 14:44:44 -0600

    s3 bucket setup and connected to content component

--- optime_subsea/src/Content.js
+++ optime_subsea/src/Content.js
@@ -1,11 +1,24 @@
-import React, { useState, useEffect } from "react";
-import { useSelector } from "react-redux";
-import styles from "./style/Contentstyle.module.css"; // Ensure the path to your styles is correct
-import { useNavigate } from "react-router-dom";
+import React from "react";
+import styles from "./style/Contentstyle.module.css"; // Ensure this path is correct
+
 const Content = () => {
+  // Function to open the AWS Management Console sign-in URL
+  const openAWSConsole = () => {
+    window.open(
+      "https://s3.console.aws.amazon.com/s3/buckets/subseaoilgas?region=us-east-2&bucketType=general&tab=objects",
+      "_blank"
+    );
+  };
+
   return (
-    <div>
-      <p>Upload your file Here</p>
+    <div className={styles.content}>
+      <p>Click the button to sign in to the AWS IAM A User</p>
+      <p>Accountid: 724465909256</p>
+      {/* Button to open AWS Management Console sign-in page */}
+      <div className={styles.buttonContainer}>
+        <button onClick={openAWSConsole}>Upload File</button>
+        <button>Analyze</button>
+      </div>
     </div>
   );
 };
